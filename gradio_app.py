82eadc4c8e76d521fa81ad9dc7968bfcb9109117
usamabjw <usamaarif126@gmail.com>
2024-03-05 13:52:45 +0500

    added image sepia feacture

--- gradio_app.py
+++ gradio_app.py
@@ -1,12 +1,15 @@
+import numpy as np
 import gradio as gr
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * intensity
+def sepia(input_img):
+    sepia_filter = np.array([
+        [0.393, 0.769, 0.189], 
+        [0.349, 0.686, 0.168], 
+        [0.272, 0.534, 0.131]
+    ])
+    sepia_img = input_img.dot(sepia_filter.T)
+    sepia_img /= sepia_img.max()
+    return sepia_img
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", gr.Slider(value=2, minimum=1, maximum=10, step=1)],
-    outputs=[gr.Textbox(label="greeting", lines=3)],
-)
-
+demo = gr.Interface(sepia, gr.Image(), "image")
 demo.launch()
