977de731a57937dd3d457708d4024ed83c33963c
usamabjw <usamaarif126@gmail.com>
2024-03-06 15:48:09 +0500

    made correction to interface

--- gradio_app.py
+++ gradio_app.py
@@ -14,13 +14,23 @@ def sepia(input_img):
     sepia_img /= sepia_img.max()
     return sepia_img
 
-demo = gr.Interface(
-    fn=[greet, sepia],  # Combine both functions
-    inputs=[
-        ["text", gr.Slider(value=2, minimum=1, maximum=10, step=1)],
-        gr.Image()
-    ],
-    outputs=[gr.Textbox(label="greeting", lines=3), gr.Image()],
-)
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            greeting = gr.Textbox(label="Name")
+            greet_intensity=gr.Slider(value=2, minimum=1, maximum=10, step=1)
+            greet_button=gr.Button(value="Greet")
+        with gr.Column():
+            out_greet= gr.Textbox(label="Greeting", lines=3)
+    with gr.Row():
+        with gr.Column():
+            in_img = gr.Image()
+            filter_button=gr.Button(value="Apply Filter")
+        with gr.Column():
+            out_img = gr.Image()
+
+    greet_button.click(greet, inputs=[greeting,greet_intensity], outputs=out_greet, api_name="greeting the user")
+    filter_button.click(sepia,inputs=in_img, outputs=out_img, api_name="applying filter")
+    
 
 demo.launch()
