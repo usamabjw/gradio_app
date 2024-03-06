7bf38e4c8cf830186090f1adc025938e82a02511
usamabjw <usamaarif126@gmail.com>
2024-03-06 12:18:57 +0500

    added img filter feature

--- gradio_app.py
+++ gradio_app.py
@@ -1,6 +1,9 @@
 import numpy as np
 import gradio as gr
 
+def greet(name, intensity):
+    return "Hello, " + name + "!" * intensity
+
 def sepia(input_img):
     sepia_filter = np.array([
         [0.393, 0.769, 0.189], 
@@ -11,5 +14,13 @@ def sepia(input_img):
     sepia_img /= sepia_img.max()
     return sepia_img
 
-demo = gr.Interface(sepia, gr.Image(), "image")
+demo = gr.Interface(
+    fn=[greet, sepia],  # Combine both functions
+    inputs=[
+        ["text", gr.Slider(value=2, minimum=1, maximum=10, step=1)],
+        gr.Image()
+    ],
+    outputs=[gr.Textbox(label="greeting", lines=3), gr.Image()],
+)
+
 demo.launch()
